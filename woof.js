6ab3cace379ec840006e508efb801210ce474acc
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2020-07-26 17:07:16 -0500

    Add files via upload

--- woof.js
+++ woof.js
@@ -26,35 +26,36 @@ client.on(`message`, msg => {
         level: 0,
         command: 0,
         data: {
-            guild: woof.cleanPropertyGuilds(woof.propertyTranslate(settings.properties, require("./storedData/guilds.json")[msg.guild.id], 2)),
-            user: woof.cleanPropertyUsers(woof.propertyTranslate(settings.properties, require("./storedData/users.json")[msg.author.id], 2)),
+            guild: woof.cleanPropertyGuilds(woof.properties(settings.properties, require("./storedData/guilds.json")[msg.guild.id], settings.propertyComs[2])),
+            user: woof.cleanPropertyUsers(woof.properties(settings.properties, require("./storedData/users.json")[msg.author.id], settings.propertyComs[2])),
         },
     };
 
-   msg.prefix = settings.prefixes.map(prefix => {return `${prefix}${settings.beta ? settings.betaPrefix.toString() : ``}`}).find(prefix => msg.content.toLowerCase().startsWith(`${prefix.toLowerCase()}`));
-   msg.level = isNaN(Number(msg.data.user.level)) ? 0 : Number(msg.data.user.level);
+    msg.prefix = settings.prefixes.map(prefix => {return `${prefix}${settings.beta ? settings.betaPrefix.toString() : ``}`}).find(prefix => msg.content.toLowerCase().startsWith(`${prefix.toLowerCase()}`));
+    msg.level = isNaN(Number(msg.data.user.level)) ? 0 : Number(msg.data.user.level);
 
     if (!(msg.prefix === undefined)){
         msg.content = msg.content.slice(msg.prefix.length, msg.content.length);
         msg.content = msg.content.startsWith(" ") ? msg.content.slice(1, msg.content.length) : msg.content;
         msg.args = msg.content.split(" ").filter(item => typeof item === "string" ? item.length > 0 : false);
         if (msg.args.length > 0){
+            msg.content = msg.content.substr(msg.args[0].length+1, msg.content.length);
             msg.command = commands.commands.find(cmd => cmd.commands.find(callers => callers.toLowerCase() === msg.args[0].toLowerCase()));
         };
     };
 
     if (!(msg.raw.author.bot)){
         if (typeof msg.command === "object" && (typeof msg.command === "object" ? msg.command.useable : false)){
-            if (msg.data.guild.channelBots.length === 0 || msg.data.guild.channelBots.some(item => item === msg.channelID)){
+            if (Array.isArray(msg.data.guild.channelBots) ? msg.data.guild.channelBots.length === 0 || msg.data.guild.channelBots.some(item => item === msg.channelID) : true){
                 if (typeof msg.command.execute === "function" && typeof msg.command === "object"){
                     if (msg.level >= msg.command.level){
                         msg.command.execute(client, msg);
                     }else{
-                        msg.raw.channel.send(`${woof.emote(emotes.ul.error)} **The permission levels on this command prevent you from using it**`)
+                        msg.raw.channel.send(`${woof.emote(emotes.ul.error)} **${settings.errors.permission}**`)
                     };
                 };
-            }else if (msg.data.guild.channelBots.length > 0){
-                msg.raw.channel.send(`${woof.emote(emotes.ul.error)} **I do not have permission to use commands here**`);
+            }else if (Array.isArray(msg.data.guild.channelBots) ? msg.data.guild.channelBots.length > 0 : false){
+                msg.raw.channel.send(`${woof.emote(emotes.ul.error)} **${settings.errors.notBotChannel}**`);
             };
         }else if (Number(msg.data.guild.channelRequest) > 0 && msg.channelID === msg.data.guild.channelRequest && typeof msg.content.length > 0){
             let commandRequest = commands.commands.find(cmd => cmd.commands.find(callers => callers.toLowerCase() === "request"));
@@ -63,7 +64,7 @@ client.on(`message`, msg => {
                     commandRequest.execute(client, msg);
                 };
             };
-        }else if (msg.data.guild.channelCountings.some(item => item === msg.channelID)){
+        }else if (Number(msg.data.guild.channelCountings) > 0 ? msg.data.guild.channelCountings.some(item => item === msg.channelID) : false){
             if (!(Number(msg.raw.content) > 0 && Math.floor(Number(msg.raw.content)) === Number(msg.raw.content))){
                 setTimeout(function(){msg.raw.delete().catch(err => {});}, 1250);
             };
