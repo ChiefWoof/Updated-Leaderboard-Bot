31e8414cf7fb4103214440c2470fc2bd6555eba1
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-09-29 18:33:25 -0500

    ADDED: BitField utility

---
+++ Bot Version 2/src/util/BitField.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/**
+ * @description A class to simplify bit-style options. Inspired by Discord.js
+ */
+
+class BitField {
+
+    constructor(value=BitField.DEFAULT_VALUE) {
+
+        /**
+         * @description The bitfield value
+         * @type {number}
+         */
+
+        this.value = value;
+
+    }
+
+    /**
+     * @returns {string} The binary string of the bitfield value
+     */
+
+    get binary() { return (this.value << 0).toString(2); }
+    
+    /**
+     * @param {number} value bitfield value 
+     * @returns {boolean} Whether the entered value can be found within the instance's value
+     */
+
+    has(value) { return (this.value & value) === value; }
+
+    /**
+     * @description Sets the bitfield value to its default value
+     */
+
+    reset() { this.value = BitField.DEFAULT_VALUE; return this; }
+
+}
+
+/**
+ * @description The individual notch
+ * @type {Object}
+ */
+
+BitField.INDICATORS = {};
+
+/**
+ * @description The default value of the bitfield
+ * @type {number}
+ */
+
+BitField.DEFAULT_VALUE = 0;
+
+module.exports = BitField;
