ebebc41b433329ea61c60cad9b307a3b8a05d036
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-13 12:10:22 -0500

    Update IconSet.js

--- Bot Version 2/configurations/ul/util/IconSet.js
+++ Bot Version 2/configurations/ul/util/IconSet.js
@@ -97,6 +97,35 @@ class IconSet {
 
         this.glowStatus = false;
 
+        /**
+         * @description The identification number of the gamemode the user selected for use
+         * * `1n` - Cube
+         * * `2n` - Ship
+         * * `3n` - Ball
+         * * `4n` - UFO
+         * * `5n` - Dart
+         * * `6n` - Robot
+         * * `7n` - Spider
+         * @type {BigInt}
+         */
+
+        this.gamemodeSelectedID = 0n;
+
+        /**
+         * @description The identification number of the gamemode to override with or 0n
+         * * `0n` - Selected gamemode
+         * * `1n` - Cube
+         * * `2n` - Ship
+         * * `3n` - Ball
+         * * `4n` - UFO
+         * * `5n` - Dart
+         * * `6n` - Robot
+         * * `7n` - Spider
+         * @type {BigInt}
+         */
+
+        this.gamemodeOverrideID = 0n;
+
     }
 
     /**
@@ -106,7 +135,7 @@ class IconSet {
      */
 
     get cube() { return this.cubeID || 1n; }
-    set cube(value) { this.cubeID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set cube(value) { this.cubeID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the ship or the default ship
@@ -115,7 +144,7 @@ class IconSet {
      */
 
     get ship() { return this.shipID || 1n; }
-    set ship(value) { this.shipID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set ship(value) { this.shipID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the ball or the default ball
@@ -124,7 +153,7 @@ class IconSet {
      */
 
     get ball() { return this.ballID || 1n; }
-    set ball(value) { this.ballID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set ball(value) { this.ballID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the ufo or the default ufo
@@ -133,7 +162,7 @@ class IconSet {
      */
 
     get ufo() { return this.ufoID || 1n; }
-    set ufo(value) { this.ufoID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set ufo(value) { this.ufoID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the dart or the default dart
@@ -142,7 +171,7 @@ class IconSet {
      */
 
     get dart() { return this.dartID || 1n; }
-    set dart(value) { this.dartID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set dart(value) { this.dartID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description (Another index for "dart") The identification number of the dart or the default dart
@@ -160,7 +189,7 @@ class IconSet {
      */
 
     get robot() { return this.robotID || 1n; }
-    set robot(value) { this.robotID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set robot(value) { this.robotID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the spider or the default spider
@@ -169,7 +198,7 @@ class IconSet {
      */
 
     get spider() { return this.spiderID || 1n; }
-    set spider(value) { this.spiderID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set spider(value) { this.spiderID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the primary icon color or the default primary color
@@ -178,7 +207,7 @@ class IconSet {
      */
 
     get color1() { return this.color1ID || 1n; }
-    set color1(value) { this.color1ID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set color1(value) { this.color1ID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the secondary icon color or the default secondary color
@@ -187,7 +216,7 @@ class IconSet {
      */
 
     get color2() { return this.color2ID || 1n; }
-    set color2(value) { this.color2ID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set color2(value) { this.color2ID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the icon trail or the default icon trail
@@ -196,7 +225,7 @@ class IconSet {
      */
 
     get trail() { return this.trailID || 1n; }
-    set trail(value) { this.trailID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set trail(value) { this.trailID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description The identification number of the icon death effect or the default death effect
@@ -205,7 +234,7 @@ class IconSet {
      */
 
     get deathEffect() { return this.deathEffectID || 1n; }
-    set deathEffect(value) { this.deathEffectID = /^[0-9]$/.test(value) ? BigInt(value) : 0n; }
+    set deathEffect(value) { this.deathEffectID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
 
     /**
      * @description Whether the icon glow is enabled
@@ -216,33 +245,93 @@ class IconSet {
     get glow() { return this.glowStatus; }
     set glow(value) { this.glowStatus = value > 0 || /true/i.test(value); }
 
-    stringfy() {
+    /**
+     * @description The identification number of the gamemode the user selected for use
+         * * `1n` - Cube
+         * * `2n` - Ship
+         * * `3n` - Ball
+         * * `4n` - UFO
+         * * `5n` - Dart
+         * * `6n` - Robot
+         * * `7n` - Spider
+     * @param {BigInt|number|string} value
+     * @type {BigInt}
+     */
+
+    get gamemodeSelected() { return this.gamemodeSelectedID || 1n; }
+    set gamemodeSelected(value) { this.gamemodeSelectedID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
+
+    /**
+     * @description The identification number of the gamemode to override with or 0n
+         * * `0n` - Selected gamemode
+         * * `1n` - Cube
+         * * `2n` - Ship
+         * * `3n` - Ball
+         * * `4n` - UFO
+         * * `5n` - Dart
+         * * `6n` - Robot
+         * * `7n` - Spider
+     * @param {BigInt|number|string} value
+     * @type {BigInt}
+     */
+
+    get gamemodeOverride() { return this.gamemodeOverrideID || 1n; }
+    set gamemodeOverride(value) { this.gamemodeOverrideID = /^[0-9]{1,}$/.test(value) ? BigInt(value) : 0n; }
+
+    /**
+     * @description The identification number of the icon death effect or the default death effect
+     * @param {BigInt|number|string} value
+     * @type {BigInt}
+     */
+
+    get gamemode() { return this.gamemodeOverrideID || this.gamemodeSelected; }
+
+    stringfy(data=this, { separator=":" }={}) {
         return [
 
-            // Icons 1 - 19
-            1, this.cubeID,
-            2, this.shipID,
-            3, this.ballID,
-            4, this.ufoID,
-            5, this.dartID,
-            6, this.robotID,
-            7, this.spiderID,
+            // Gamemodes 1 - 19
+            1, data.cubeID,
+            2, data.shipID,
+            3, data.ballID,
+            4, data.ufoID,
+            5, data.dartID,
+            6, data.robotID,
+            7, data.spiderID,
+
+            18, data.gamemodeSelectedID,
+            19, data.gamemodeOverrideID, 
 
             // Colors 20 - 29
-            20, this.color1ID,
-            21, this.color2ID,
+            20, data.color1ID,
+            21, data.color2ID,
 
             // Details 30 - 39
-            30, this.trailID,
-            31, this.deathEffectID,
-            32, this.glowStatus ? 1 : 0,
+            30, data.trailID,
+            31, data.deathEffectID,
+            32, data.glowStatus ? 1 : 0,
 
-        ].join(":");
+        ].join(separator);
     }
 
-    parse(data) {
+    parse(data, { separator=":" }={}) {
         if (typeof data === "string") {
-            data = data.split(":").reduce((v, a, i) => {
+            // "#" is to easily convert between how icon data used to be stored on the UL
+            if (data.includes("#")) {
+                data = data.split("#");
+                if (data[0]) {
+                    data[0] = data[0].replace(/98:\d{0,}:99:\d{0,}$/, `19:${data[0].match(/\d{0,}:99:\d{0,}$/)}`);
+                    data[0] = data[0].replace(/99:\d{0,}$/, `18:${data[0].match(/\d{0,}$/)}`);
+                }
+                if (data[1]) {
+                    data[1] = data[1].replace(/^1:/, "20:");
+                    data[1] = data[1].replace(/2:\d{0,}$/, `21:${data[1].match(/\d{0,}$/)}`);
+                }
+                if (data[2]) {
+                    data[2] = data[2].replace(/^(0|1):/, "32:");
+                }
+                data = data.join(separator);
+            }
+            data = data.split(separator).reduce((v, a, i) => {
                 if (i % 2 === 0) v.key = a;
                 else v.data[v.key] = a;
                 return v;
@@ -261,6 +350,8 @@ class IconSet {
             else if (/^(5|dart(ID)?|wave(ID)?)$/i.test(k)) this.dart = v;
             else if (/^(6|robot(ID)?)$/i.test(k)) this.robot = v;
             else if (/^(7|spider(ID)?)$/i.test(k)) this.spider = v;
+            else if (/^(18|gamemode(ID)?|gamemodeSelected(ID)?|gamemodeSelection(ID)?)$/i.test(k)) this.gamemodeSelected = v;
+            else if (/^(19|gamemodeOverride(ID)?|gamemodeOverwrite(ID)?)$/i.test(k)) this.gamemodeOverride = v;
             else if (/^(20|color1(ID)?)$/i.test(k)) this.color1 = v;
             else if (/^(21|color2(ID)?)$/i.test(k)) this.color2 = v;
             else if (/^(30|trail(ID)?)$/i.test(k)) this.trail = v;
