914d4ab8bb3ff58a645f18ce2fd0727a22550309
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-15 23:27:53 -0500

    Color adjustments and presets

--- Bot Version 2/src/util/Color.js
+++ Bot Version 2/src/util/Color.js
@@ -97,12 +97,85 @@ class Color extends BitField {
             super.resolve(bit);
     }
 
+    brighten(percentage=0) {
+        if (["string", "number", "bigint"].includes(typeof percentage) && /^\d{1,}$/.test(`${percentage}`.replace(".", ""))) {
+            percentage = Number(percentage) / 100;
+            this.R = Math.max(0, Math.min(255, Math.round(this.R + 255 * percentage)));
+            this.G = Math.max(0, Math.min(255, Math.round(this.G + 255 * percentage)));
+            this.B = Math.max(0, Math.min(255, Math.round(this.B + 255 * percentage)));
+        }
+        return this;
+    }
+
+    darken(percentage=0) {
+        if (["string", "number", "bigint"].includes(typeof percentage) && /^\d{1,}$/.test(`${percentage}`.replace(".", ""))) {
+            percentage = Number(percentage) / 100;
+            this.R = Math.max(0, Math.min(255, Math.round(this.R - 255 * percentage)));
+            this.G = Math.max(0, Math.min(255, Math.round(this.G - 255 * percentage)));
+            this.B = Math.max(0, Math.min(255, Math.round(this.B - 255 * percentage)));
+        }
+        return this;
+    }
+
+    inverse() {
+        this.R = 255 - this.R;
+        this.G = 255 - this.G;
+        this.B = 255 - this.B;
+        return this;
+    }
+
 }
 
 Color.BASES = {
     R: ((1 << 24) - 1) ^ ((1 << 16) - 1),
     G: ((1 << 16) - 1) ^ ((1 << 8) - 1),
     B: (1 << 8) - 1,
+};
+
+/**
+ * @description Preset colors
+ */
+
+Color.PRESETS = {
+    RED: new Color("FF0000"),
+        DARK_RED: new Color("FF0000").darken(25),
+        LIGHT_RED: new Color("FF0000").brighten(25),
+    ORANGE: new Color("FF681F"),
+        DARK_ORANGE: new Color("FF681F").darken(12.5),
+        LIGHT_ORANGE: new Color("FF681F").brighten(12.5),
+    YELLOW: new Color("FFFF00"),
+        DARK_YELLOW: new Color("FFFF00").darken(12.5),
+        LIGHT_YELLOW: new Color("FFFF00").brighten(36),
+    GREEN: new Color("00FF00"),
+        DARK_GREEN: new Color("00FF00").darken(25),
+        LIGHT_GREEN: new Color("00FF00").brighten(36),
+    CYAN: new Color("00FFFF"),
+        DARK_CYAN: new Color("00FFFF").darken(12.5),
+        LIGHT_CYAN: new Color("00FFFF").brighten(50),
+    BLUE: new Color("0000FF"),
+        DARK_BLUE: new Color("0000FF").darken(25),
+        LIGHT_BLUE: new Color("0000FF").brighten(25),
+    MAGENTA: new Color("FF00FF"),
+        DARK_MAGENTA: new Color("FF00FF").darken(25),
+        LIGHT_MAGENTA: new Color("FF00FF").brighten(25),
+    PURPLE: new Color("660099"),
+        DARK_PURPLE: new Color("660099").darken(17.5),
+        LIGHT_PURPLE: new Color("660099").brighten(20),
+    BROWN: new Color("964B00"),
+        DARK_BROWN: new Color("964B00").darken(10),
+        LIGHT_BROWN: new Color("964B00").brighten(10),
+
+    BLACK: new Color(0),
+        LIGHT_BLACK: new Color(0).brighten(15),
+    WHITE: new Color("FFF"),
+        DARK_WHITE: new Color("FFF").darken(5),
+
+    GREY: new Color("FFF").darken(50),
+        DARK_GREY: new Color("FFF").darken(75),
+        LIGHT_GREY: new Color("FFF").darken(25),
+    GRAY: new Color("FFF").darken(50),
+        DARK_GRAY: new Color("FFF").darken(75),
+        LIGHT_GRAY: new Color("FFF").darken(25)
 };
 
 module.exports = Color;
