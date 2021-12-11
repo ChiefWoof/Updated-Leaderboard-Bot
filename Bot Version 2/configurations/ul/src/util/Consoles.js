06237e461442bc72bdd7d67e338adb6a4f4c1c5b
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-12-11 00:34:53 -0600

    IOS, Android, and Windows

--- Bot Version 2/configurations/ul/src/util/Consoles.js
+++ Bot Version 2/configurations/ul/src/util/Consoles.js
@@ -10,29 +10,93 @@ const BitField = require("./BitField");
 class Consoles extends BitField {
 
     /**
-     * @description Whether the user plays on mobile
+     * @description Whether the user uses any mobile device
      * @type {boolean}
      * @param {boolean} bool
      */
 
-    get MOBILE() { return this.has(this.indicators.MOBILE); }
-    set MOBILE(bool) { return this.resolveBitBoolean(this.indicators.MOBILE, bool); }
+    get mobile() { return this.has(this.indicators.MOBILE); }
+    set mobile(bool) { return this.resolveBitBoolean(this.indicators.MOBILE, bool); }
 
     /**
-     * @description Whether the user plays on PC
+     * @description Whether the user uses an IOS mobile device (iPhone, iPad, iPod, etc...)
      * @type {boolean}
      * @param {boolean} bool
      */
 
-    get PC() { return this.has(this.indicators.PC); }
-    set PC(bool) { return this.resolveBitBoolean(this.indicators.PC, bool); }
+    get mobileIOS() { return this.has(this.indicators.MOBILE_IOS); }
+    set mobileIOS(bool) { return this.resolveBitBoolean(this.indicators.MOBILE_IOS, bool); }
+
+    /**
+     * @description Whether the user uses an Android mobile device
+     * @type {boolean}
+     * @param {boolean} bool
+     */
+
+    get mobileAndroid() { return this.has(this.indicators.MOBILE_ANDROID); }
+    set mobileAndroid(bool) { return this.resolveBitBoolean(this.indicators.MOBILE_ANDROID, bool); }
+
+    /**
+     * @description Whether the user uses any mobile device
+     * @type {boolean}
+     * @param {boolean} bool
+     */
+
+    get pc() { return this.has(this.indicators.PC); }
+    set pc(bool) { return this.resolveBitBoolean(this.indicators.PC, bool); }
+
+    /**
+     * @description Whether the user uses an IOS pc device (Mac)
+     * @type {boolean}
+     * @param {boolean} bool
+     */
+
+    get pcIOS() { return this.has(this.indicators.PC_IOS); }
+    set pcIOS(bool) { return this.resolveBitBoolean(this.indicators.PC_IOS, bool); }
+
+    /**
+     * @description Whether the user uses an Android mobile device
+     * @type {boolean}
+     * @param {boolean} bool
+     */
+
+    get pcWindows() { return this.has(this.indicators.PC_WINDOWS); }
+    set pcWindows(bool) { return this.resolveBitBoolean(this.indicators.PC_WINDOWS, bool); }
+
+    /**
+     * @description Updates the value of the general mobile bit if necessary
+     */
+
+    patch_mobile() {
+        this.mobile = this.mobile
+        || this.mobileIOS
+        || this.mobileAndroid;
+    }
+
+    /**
+     * @description Updates the value of the general pc bit if necessary
+     */
+
+    patch_pc() {
+        this.pc = this.pc
+        || this.pcWindows
+        || this.pcIOS;
+    }
 
 }
 
 Consoles.INDICATORS = {
 
+    // MOBILE (0-7);
     MOBILE: 1 << 0,
-    PC: 1 << 2
+    MOBILE_IOS: 1 << 1,
+    MOBILE_ANDROID: 1 << 2,
+
+    // PC (8-15)
+    PC: 1 << 8,
+    PC_WINDOWS: 1 << 9,
+    PC_IOS: 1 << 10
+
 
 };
 
